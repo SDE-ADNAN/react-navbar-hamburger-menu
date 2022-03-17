b286ba06bb9204291cc4e63079dde34db4660c75
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-17 13:00:04 +0530

    added the logic for button component

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Button.css'
-const Button = props => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
 
+const STYLES=[
+    'btn--primary',
+    'btn--outline',
+]
 
-export default Button;
+const SIZES=[
+    'btn--medium',
+    'btn--large',
+]
+
+export const Button =({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+})=>{
+        const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle:STYLES[0]
+
+        const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize:SIZES[0]
+        return(<>
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} type={type} onClick={onClick}>
+            {children}
+        </button>
+        </>)
+}
